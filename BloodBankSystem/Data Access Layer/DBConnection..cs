58dacac3f56561a0ec7181df34bdea1053b34951
salsabil <salsabilalimahmoud@gmail.com>
2026-05-03 22:27:17 +0300

    refactor: moved tier folders into main project folder for compilation

---
+++ BloodBankSystem/Data Access Layer/DBConnection..cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace  BloodBankSystem.DataAccessLayer
+{
+    public class DBConnection
+    {
+        // This pulls the string from  App.config file
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["HemoGlobeDB"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
