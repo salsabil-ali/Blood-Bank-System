3d9f831c36f92529f2702677cd4a5b51c51b5afc
Omnia Magdy <omniamagdy7801@gmail.com>
2026-05-07 15:48:55 +0300

    edits

--- BloodBankManagmentSystem/Presentation Layer/Form1.cs
+++ BloodBankManagmentSystem/Presentation Layer/Form1.cs
@@ -14,18 +14,42 @@ namespace BloodBankManagmentSystem.Presentation_Layer
         {
             InitializeComponent();
         }
-
+        // button1 = Donor Management
         private void button1_Click_1(object sender, EventArgs e)
         {
             Form2 myf = new Form2();
             myf.Show();
-
         }
 
+        // button2 = Donation / Blood Unit
         private void button2_Click(object sender, EventArgs e)
         {
             Form3 myf = new Form3();
             myf.Show();
+        }
+
+        // button3 = Hospital Requests
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 myf = new Form4();
+            myf.Show();
+        }
+
+        // button4 = Inventory (Blood Units)
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form5 myf = new Form5();
+            myf.Show();
+        }
+
+        // button5 = Exit
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to exit?", "Exit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -52,6 +76,11 @@ namespace BloodBankManagmentSystem.Presentation_Layer
         }
 
         private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
         {
 
         }
--- BloodBankManagmentSystem/Presentation Layer/Form2.cs
+++ BloodBankManagmentSystem/Presentation Layer/Form2.cs
@@ -51,5 +51,10 @@ namespace BloodBankManagmentSystem.Presentation_Layer
         {
 
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
