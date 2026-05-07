24ebc297e4b37cf3e64ce9c505e3a7a2991d2ed8
salsabil <salsabilalimahmoud@gmail.com>
2026-05-07 17:08:12 +0300

    bloodType function

--- BloodBankManagmentSystem/Models/Donor.cs
+++ BloodBankManagmentSystem/Models/Donor.cs
@@ -8,7 +8,15 @@ namespace BloodBankManagmentSystem.Models
         public string Name { get; set; }
         public string Gender { get; set; }
         public DateTime Date_of_Birth { get; set; }
+
+        
         public string Blood_Type { get; set; }
+        public string BloodType
+        {
+            get => Blood_Type;
+            set => Blood_Type = value;
+        }
+
         public string Phone_Number { get; set; }
         public string Address { get; set; }
     }
